716b5c3bd1764797c44b005f46c6d99f584b6658
Manish <manishvm310@gmail.com>
2026-05-18 16:32:26 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['admin', 'employee'],
+      enum: ['admin', 'employee', 'vendor'],
       default: 'employee',
     },
 
