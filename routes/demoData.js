82ba8ffff5956de82c89f2db51cc5e601b5629bb
Manish <manishvm310@gmail.com>
2026-05-18 16:35:08 +0530

    Update demoData.js

--- routes/demoData.js
+++ routes/demoData.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const demoDataController = require('../controllers/demoDataController');
 const { verifyToken, requireRole } = require('../middleware/auth');
 
-router.post('/', verifyToken, requireRole(['admin', 'employee']), demoDataController.createDemoData);
+router.post('/', verifyToken, requireRole(['admin', 'employee', 'vendor']), demoDataController.createDemoData);
 router.get('/', verifyToken, demoDataController.getAllDemoData);
 router.get('/:partNo', verifyToken, demoDataController.getDemoData);
 
